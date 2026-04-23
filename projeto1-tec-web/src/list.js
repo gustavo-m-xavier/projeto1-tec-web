285bb43deac78048ee64ef0d61925a4b05cabe2f
Charles Santos <coliveirasantos1234@gmail.com>
2026-04-22 22:30:58 -0300

    .

--- projeto1-tec-web/src/list.js
+++ projeto1-tec-web/src/list.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const data = JSON.parse(localStorage.getItem("data")) || [];
 
 const lista = document.getElementById("lista");
@@ -16,22 +15,3 @@ data.map((contato) => {
 
   lista.appendChild(li);
 });
-=======
-const data = JSON.parse(localStorage.getItem("data")) || [];
-
-const lista = document.getElementById("lista");
-
-data.map((contato) => {
-  const li = document.createElement("li");
-
-  li.innerHTML = `
-    <strong>${contato.name}</strong>
-    <span class="info">
-      <span><strong>Email:</strong> ${contato.email}</span>
-      <span><strong>Telefone:</strong> ${contato.phone}</span>
-    </span>
-  `;
-
-  lista.appendChild(li);
-});
->>>>>>> 8eee66090b01646a12e9cd496248f339a553451b
